b7ed9003244040d46ccef697bef08c014d3894ef
Allan Yoshio Hasegawa <hasegawa.aran@gmail.com>
2013-07-23 03:40:11 -0300

    Point fitting sample now uses the generic interface; License info included;

--- samples/point_fitting/point_fitting.cpp
+++ samples/point_fitting/point_fitting.cpp
@@ -1,9 +1,39 @@
+/*
+ Copyright 2012 Allan Yoshio Hasegawa
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ -----------------------------------------------------------------------------
+ */
+
+/*
+
+Point Fitting Sample
+
+-> Useless algorithm. Tries to find the position of a point
+	using differential evolution :P
+-> It's a simple thing to hunt for bugs
+-> Can be used as a framework to expand to others problems
+
+*/
+
+
 #include <cstdio>
 #include <random>
 #include <array>
 #include <tuple>
 #include <cmath>
 
+#include "BaseDE.hpp"
 #include "SequentialDE.hpp"
 
 constexpr std::array<double,2> POINT {{23.345, -10.009}};
@@ -18,6 +48,7 @@ constexpr double DOMAIN_LIMITS = 128;
 
 int main(int argc, char *argv[]) {
   using pdebc::SequentialDE;
+  using pdebc::BaseDE;
   using namespace std;
 
   random_device rd;
@@ -27,8 +58,6 @@ int main(int argc, char *argv[]) {
 
   uniform_int_distribution<uint32_t> up(0,POPULATION_SIZE);
   auto rand_pop = bind(up, emt);
-
-
 
   auto calc_error = [](const std::array<POPULATION_TYPE, POPULATION_DIM>& arr) -> ERROR_TYPE {
     double e = std::sqrt(
@@ -42,23 +71,23 @@ int main(int argc, char *argv[]) {
   auto error_evaluation = [](const ERROR_TYPE& a, const ERROR_TYPE& b) {
     return a < b;
   };
+  
 
-  SequentialDE<POPULATION_TYPE,POPULATION_DIM,POPULATION_SIZE,ERROR_TYPE> de {
+  shared_ptr<BaseDE<POPULATION_TYPE,POPULATION_DIM,POPULATION_SIZE,ERROR_TYPE>> de =
+  make_shared<SequentialDE<POPULATION_TYPE,POPULATION_DIM,POPULATION_SIZE,ERROR_TYPE>>(
+    0.5, 0.8,
     std::move(rand_domain), //std::function<POP_TYPE()>&& callback_population_generator
     std::move(rand_pop), //std::function<uint32_t()>&& callback_population_picker
     std::move(calc_error), //std::function<ERROR_TYPE(const std::array<POP_TYPE,POP_DIM>&)>&& callback_calc_error
     std::move(error_evaluation) //std::function<bool(const ERROR_TYPE&,const ERROR_TYPE&)>&& callback_error_evaluation)
-  };
-
-  de.kCR_ = 0.5;
-  de.kF_ = 0.8;
-
+  );
+  
   for (int i = 0; i < 20; i++) {
-    de.solveOneGeneration();
+    de->solveOneGeneration();
   }
 
-  auto bc_error = get<0>(de.getBestCandidate());
-  auto bc_point = get<1>(de.getBestCandidate());
+  auto bc_error = get<0>(de->getBestCandidate());
+  auto bc_point = get<1>(de->getBestCandidate());
   printf("POINT: (%g,%g)\n", POINT[0],POINT[1]);
   printf("Best candidate point: (%g,%g)\n", bc_point[0], bc_point[1]);
   printf("Best Candidate error: %g\n", std::sqrt(bc_error));
